0e26ff861467096379ae87a3c85f8fda4831bab5
Esther Jun Kim <esther.jun.kim@gmail.com>
2018-02-03 08:52:29 -0800

    Delete a comment in make-venv.sh
    The deleted comment was unnecessary, since it just repeats the echo commands at the end. :)

--- scripts/make-venv.sh
+++ scripts/make-venv.sh
@@ -2,7 +2,6 @@
 
 # When using Python, it's always a good idea to create a virtual environment to isolate dependencies.
 # This script uses venv to create the virtual environment in the target directory.
-# To activate, type: `source ~/.venv/ok-google-pi-zero-env/bin/activate`.
 
 mkdir -p ~/.venv
 python3 -m venv ~/.venv/ok-google-pi-zero-env
