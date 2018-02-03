2ce42b582f4e23cadcf19ff0e1d6eb2fb631eea2
Esther Jun Kim <esther.jun.kim@gmail.com>
2018-02-03 08:42:00 -0800

    Add activate-venv.sh
    This just echoes the command that you need to activate the project's virtual environment.

---
+++ scripts/activate-venv.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This script just prints out the command needed to activate the project's virtual environment.
+# TODO: Activate the virtual environment automagically.
+
+echo "To activate the virtual environment, type: "
+echo "source ~/.venv/ok-google-pi-zero-env/bin/activate"
